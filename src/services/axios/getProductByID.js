514aed304b6403f3ccfb973e91b2eed26469e410
TPLong2002 <tranphilong2301@gmail.com>
2023-12-29 09:49:20 +0700

    deploy

--- src/services/axios/getProductByID.js
+++ src/services/axios/getProductByID.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 function getProductById(id) {
-  return axios.get(`http://api.shopiec.shop/api/items/item/${id}`);
+  return axios.get(`http://api.shopiec.shop/api/items/item/${id}`, {
+    headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+  });
 }
 export default getProductById;
